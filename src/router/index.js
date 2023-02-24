f4602c67e1a355c3d3ba49ebf80ec1ab787f5212
Sumit Upadhyay <sumit.kumar.upadhyay@gmail.com>
2023-02-24 12:59:09 +0530

    added name for chunks in router

--- src/router/index.js
+++ src/router/index.js
@@ -27,27 +27,27 @@ const routes = [
   {
     path: "/authors",
     name: "authors",
-    component: () => import("../views/AuthorsView.vue"),
+    component: () => import(/* webpackChunkName: "authors" */ "../views/AuthorsView.vue"),
   },
   {
     path: "/authors/:user",
     name: "authorsDetails",
-    component: () => import("../views/AuthorsDetail.vue"),
+    component: () => import(/* webpackChunkName: "authorsdetails" */ "../views/AuthorsDetail.vue"),
   },
   {
     path: '/tag/:id',
     name: "tags",
-    component: () => import("../components/PostBy.vue"),
+    component: () => import(/* webpackChunkName: "tags" */ "../components/PostBy.vue"),
   },
   {
     path: '/category/:id',
     name: "category",
-    component: () => import("../components/PostBy.vue"),
+    component: () => import(/* webpackChunkName: "PostBy" */ "../components/PostBy.vue"),
   },
   {
     path: "/search",
     name: "Search",
-    component: () => import("../components/SearchResult.vue"),
+    component: () => import(/* webpackChunkName: "search" */ "../components/SearchResult.vue"),
   },
   {
     path: "/login",
@@ -88,31 +88,15 @@ const router = new VueRouter({
 });
 
 store.dispatch("b/login/autoLogin").then(() => {
-  // console.log('response',response);
   router.beforeEach((to, from, next) => {
-    // console.error(store)
     let isUserLoggedin = store.getters["b/login/isUserLoggedin"];
-    // console.log('to.meta.auth', to.meta.auth);
-    // console.info("isUserLoggedin", isUserLoggedin);
-    // console.warn("('auth' in to.meta)", ('auth' in to.meta));
     if (('auth' in to.meta) && to.meta.auth && !isUserLoggedin) {
-      // console.log(111);
       next('/login');
     } else if (('auth' in to.meta) && !to.meta.auth && isUserLoggedin) {
       next('/profile');
-      // console.log(222);
     } else {
       next();
-      // console.log(333);
     }
-    // if(('auth' in to.meta)  && to.meta.auth && !this.$store.getters['b/login/isUserLoggedin']){
-    //   next('/login');
-
-    // } else if(('auth' in to.meta) && !to.meta.auth && this.$store.getters['b/login/isUserLoggedin']){
-    //   next('/profile');
-    // } else {
-    //next()
-    // }
   });
 })
 export default router;  
